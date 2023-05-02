2a883851c2fa56c7ce3983df13a2df841247a453
TaRaSiSt <tarasraihel@gmail.com>
2023-05-02 15:03:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,153 @@
+function FontChangeEng() {
+    var font = document.getElementById("FCUser").value;
+    document.getElementById("ea").innerHTML = font;
+    document.getElementById("eb").innerHTML = font;
+    document.getElementById("ec").innerHTML = font;
+    document.getElementById("ed").innerHTML = font;
+    document.getElementById("ee").innerHTML = font;
+    document.getElementById("ef").innerHTML = font;
+    document.getElementById("eg").innerHTML = font;
+    document.getElementById("eh").innerHTML = font;
+    document.getElementById("ei").innerHTML = font;
+    document.getElementById("ej").innerHTML = font;
+    document.getElementById("ek").innerHTML = font;
+    document.getElementById("el").innerHTML = font;
+    document.getElementById("em").innerHTML = font;
+    document.getElementById("eo").innerHTML = font;
+    document.getElementById("eq").innerHTML = font;
+    document.getElementById("er").innerHTML = font;
+    document.getElementById("es").innerHTML = font;
+    document.getElementById("et").innerHTML = font;
+    document.getElementById("eu").innerHTML = font;
+    document.getElementById("ev").innerHTML = font;
+    document.getElementById("ew").innerHTML = font;
+    document.getElementById("ex").innerHTML = font;
+    document.getElementById("ey").innerHTML = font;
+    document.getElementById("ez").innerHTML = font;
+    document.getElementById("en").innerHTML = font;
+}
+
+function FontChangePl() {
+    var font = document.getElementById("FCUser").value;
+    document.getElementById("pa").innerHTML = font;
+    document.getElementById("pb").innerHTML = font;
+    document.getElementById("pc").innerHTML = font;
+    document.getElementById("pd").innerHTML = font;
+    document.getElementById("pe").innerHTML = font;
+    document.getElementById("pf").innerHTML = font;
+    document.getElementById("pg").innerHTML = font;
+    document.getElementById("ph").innerHTML = font;
+    document.getElementById("pi").innerHTML = font;
+    document.getElementById("pj").innerHTML = font;
+    document.getElementById("pk").innerHTML = font;
+    document.getElementById("pl").innerHTML = font;
+    document.getElementById("pm").innerHTML = font;
+    document.getElementById("pn").innerHTML = font;
+}
+
+function FontChangeUa() {
+    var font = document.getElementById("FCUser").value;
+    document.getElementById("ua").innerHTML = font;
+    document.getElementById("ub").innerHTML = font;
+    document.getElementById("uc").innerHTML = font;
+    document.getElementById("ud").innerHTML = font;
+    document.getElementById("ue").innerHTML = font;
+    document.getElementById("uf").innerHTML = font;
+    document.getElementById("ug").innerHTML = font;
+    document.getElementById("uh").innerHTML = font;
+    document.getElementById("ui").innerHTML = font;
+    document.getElementById("uj").innerHTML = font;
+    document.getElementById("uk").innerHTML = font;
+    document.getElementById("ul").innerHTML = font;
+    document.getElementById("um").innerHTML = font;
+    document.getElementById("un").innerHTML = font;
+    document.getElementById("uo").innerHTML = font;
+}
+
+function white() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#ffffff";
+    }
+}
+
+function warmWhite() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#ffdead";
+    }
+}
+
+function blue() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#0048ff";
+    }
+}
+
+function iceBlue() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#00ffff";
+    }
+}
+
+function green() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#3ef813";
+    }
+}
+
+function orange() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#ff4f00";
+    }
+}
+
+function pink() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#ff00ff";
+    }
+}
+
+function purple() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#cd00cd";
+    }
+}
+
+function red() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#f51010";
+    }
+}
+
+function darkYellow() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#ffa500";
+    }
+}
+
+function yellow() {
+    var x = document.querySelectorAll("div > li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "#fde910";
+    }
+}
